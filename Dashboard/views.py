4df95efed341275225392b3b48f303319386dbc1
21mx201 <21mx201@MCA.PSG>
2022-06-02 10:59:29 +0530

    Initial Commit

---
+++ Dashboard/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Login(request):
+    return render(request,"Dashboard/login.html")
+
+def Dashboard(request):
+    return render(request,"Dashboard/Dashboard.html")
